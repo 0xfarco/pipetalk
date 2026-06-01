50ef9e687646d35fbf71c97569972f3082080edd
Far Han Cholakkal <0xfarco@gmail.com>
2026-06-01 23:27:56 +0530

    support continuous parent-child messaging

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 #define BUFFERSIZE 100
 
+std::string getTimestamp();
+
 int main() {
     int parentToChild[2];
     int childToParent[2];
@@ -17,30 +19,39 @@ int main() {
         close(parentToChild[1]);
         close(childToParent[0]);
 
-        char buffer[BUFFERSIZE];
-        std::string msg;
+        while (1) {
+            char buffer[BUFFERSIZE] {};
+            std::string msg;
 
-        read(parentToChild[0], buffer, sizeof(buffer));
+            read(parentToChild[0], buffer, sizeof(buffer));
 
-        std::cout << "Parent: " << buffer << std::endl;
+            std::cout << "Parent: " << buffer << std::endl;
 
-        getline(std::cin, msg);
+            getline(std::cin, msg);
 
-        write(childToParent[1], msg.c_str(), msg.size() + 1);
+            write(childToParent[1], msg.c_str(), msg.size() + 1);
+        }
+        close(parentToChild[0]);
+        close(childToParent[1]);
     } else { // parent
         close(parentToChild[0]);
         close(childToParent[1]);
 
-        std::string msg;
-        char buffer[BUFFERSIZE];
+        while (1) {
+            std::string msg;
+            char buffer[BUFFERSIZE] {};
 
-        getline(std::cin, msg);
+            getline(std::cin, msg);
 
-        write(parentToChild[1], msg.c_str(), msg.size() + 1);
+            write(parentToChild[1], msg.c_str(), msg.size() + 1);
 
-        read(childToParent[0], buffer, sizeof(buffer));
+            read(childToParent[0], buffer, sizeof(buffer));
 
-        std::cout << "Child: " << buffer << std::endl;
+            std::cout << "Child: " << buffer << std::endl;
+        }
+
+        close(parentToChild[1]);
+        close(childToParent[0]);
     }
     return 0;
 }
