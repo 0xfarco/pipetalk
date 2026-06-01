0dab70b4a058743063bd5538b5502a43e17a5328
Far Han Cholakkal <0xfarco@gmail.com>
2026-06-01 23:17:27 +0530

    implement basic parent-child chat using POSIX pipes

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+
+#define BUFFERSIZE 100
+
+int main() {
+    int parentToChild[2];
+    int childToParent[2];
+
+    pipe(parentToChild);
+    pipe(childToParent);
+
+    pid_t pid = fork();
+
+    if (pid == 0) { // child
+        close(parentToChild[1]);
+        close(childToParent[0]);
+
+        char buffer[BUFFERSIZE];
+        std::string msg;
+
+        read(parentToChild[0], buffer, sizeof(buffer));
+
+        std::cout << "Parent: " << buffer << std::endl;
+
+        getline(std::cin, msg);
+
+        write(childToParent[1], msg.c_str(), msg.size() + 1);
+    } else { // parent
+        close(parentToChild[0]);
+        close(childToParent[1]);
+
+        std::string msg;
+        char buffer[BUFFERSIZE];
+
+        getline(std::cin, msg);
+
+        write(parentToChild[1], msg.c_str(), msg.size() + 1);
+
+        read(childToParent[0], buffer, sizeof(buffer));
+
+        std::cout << "Child: " << buffer << std::endl;
+    }
+    return 0;
+}
